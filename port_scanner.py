d89987df74f4e35039bc3beda0f5b24b945cbd97
Margaret Nieves Gonzales <46605241-margaretnieves1@users.noreply.replit.com>
2025-08-22 05:01:43 +0000

    Made it check if error is caused by icorrect IP address or incorrect Hostname

--- port_scanner.py
+++ port_scanner.py
@@ -1,39 +1,23 @@
 import socket
 import ipaddress
+import re
 from common_ports import ports_and_services
 
 
 def get_open_ports(target, port_range, verbose=False):
     open_ports = []
 
-    try:
+    ip = None
+
+    if re.search(r'[a-zA-Z]', target):
+        try:
+            ip = socket.gethostbyname(target)
+        except socket.gaierror:
+            return "Error: Invalid hostname"
+    else:
         try:
             ip = str(ipaddress.ip_address(target))
-            hostname = target
-        except ValueError:
-            try:
-                ip = str(ipaddress.ip_address(target))
-                hostname = target
-            except socket.gaierror:
-                return "Error: Invalid hostname"
-    except Exception:
-        return "Error: Invalid IP address"
+        except socket.error:
+            return "Error: Invalid IP address"
 
-    for port in range(port_range[0], port_range[1] + 1):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
-
-        result = s.connect_ex((ip, port))
-        if result == 0:
-            open_ports.append(port)
-        s.close()
-
-    if verbose:
-        result = f"Open ports for {hostname} ({ip})\n"
-        result += "PORT     SERVICE\n"
-        for port in open_ports:
-            service = ports_and_services.get(port, "unknown")
-            result += f"{port:<8}{service}\n"
-        return result.strip()
-    else:
-        return (open_ports)
+    return (open_ports)
